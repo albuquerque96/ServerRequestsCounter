b20a9d28a4a7235748c3c01aec2b4bda18150862
albuquerque96 <88738051+albuquerque96@users.noreply.github.com>
2024-05-25 00:53:34 +0100

    phase1 completed needs further testing

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+var totalCalls = 0;
+
+ const  callCounter = async(req, res, next) => {
+  ++totalCalls;
+  console.log(totalCalls);
+  next();
+};
+
+app.get('/',callCounter, (req, res) => {
+  res.status(200).json({ "total": totalCalls });
+});
+
+app.use(callCounter);
+
+// Use process.argv to get command line arguments
+const port = process.argv[2] || 5000; // Default to 5000 if no port is provided
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
